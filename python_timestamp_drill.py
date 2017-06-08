00cd6e1f4b59a2e8f5ccb51957aff08574305bd6
rianjeong <rianjeong@yahoo.com>
2017-06-08 15:11:02 -0700

    this is the new commit
    Here is the new python timestamp drill with out a bunch of the ## that I was trying during development.

---
+++ python_timestamp_drill.py
@@ -0,0 +1,27 @@
+import shutil
+import os
+import time
+from datetime import timedelta, datetime
+
+source = 'C:/Users/rianjeong/Desktop/Folder_A'
+destination = 'C:/Users/rianjeong/Desktop/Folder_B'
+now = time.time()
+diff_sec = float(24*3600)
+before = now - diff_sec
+
+
+for files in os.listdir(source):
+    src_file = os.path.join(source, files)
+    dst_file = os.path.join(destination, files)
+    st = os.stat(src_file)
+
+    mod_time = datetime.fromtimestamp(st.st_mtime)
+    new_time = time.mktime(mod_time.timetuple())
+    if new_time > before:
+        shutil.copy(src_file, dst_file)
+
+
+
+    
+
+    
